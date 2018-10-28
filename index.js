ae29aedb92322e90b36b2653189bcf410330b6fb
 juthakarn saelim <juthakarnsaelim@MacBook-Air-khxng-juthakarn.local>
2018-10-29 01:43:25 +0700

    install body-parser

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.listen(port, () => {
     console.log('Sever Listen on port', port)
