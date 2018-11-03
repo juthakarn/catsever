81b2131a9355332d304f80f5417d91e83be6fe59
Kittinut Pramhan <knightza123@hotmail.com>
2018-11-04 03:34:01 +0700

    fix babel preset

---
+++ server/routes/authentication.js
@@ -0,0 +1,17 @@
+export default (app,route)=>{
+    app.get('/',(req,res)=>{
+        res.send('helloworld')
+    })
+    
+    app.post('/user',(req,res)=>{
+        //localhost:3000/user method post
+      /*{
+          name:'something',
+          surname:'something'
+        }*/     
+        const {name,surname} = req.body;
+        console.log(name)
+        res.send({hello:name})
+    
+    })
+}
