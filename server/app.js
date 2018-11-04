28e7b4a9a01997f00890e9efb24821d5d543d1f2
 juthakarn saelim <juthakarnsaelim@MacBook-Air-khxng-juthakarn.local>
2018-11-05 01:23:28 +0700

    docker-compose

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import authenticationRoute from './routes/authentication'
+import hospitalSevice from './routes/hospitalservice'
 const app = express()
 const port = 3000
 
@@ -8,15 +9,14 @@ const port = 3000
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
-app.use(bodyParser.json()) 
+app.use(bodyParser.json())
+
 
 
 /*Router */
 authenticationRoute(app)
-
+hospitalSevice(app)
 
 app.listen(port, () => {
     console.log('Sever Listen on port', port)
 })
-
-
--- server/routes/authentication.js
+++ server/routes/authentication.js
@@ -1,17 +1,17 @@
-export default (app,route)=>{
-    app.get('/',(req,res)=>{
+export default (app, route) => {
+    app.get('/', (req, res) => {
         res.send('helloworld')
     })
-    
-    app.post('/user',(req,res)=>{
+
+    app.post('/user', (req, res) => {
         //localhost:3000/user method post
-      /*{
-          name:'something',
-          surname:'something'
-        }*/     
-        const {name,surname} = req.body;
+        /*{
+            name:'something',
+            surname:'something'
+          }*/
+        const { name, surname } = req.body;
         console.log(name)
-        res.send({hello:name})
-    
+        res.send({ hello: name })
+
     })
 }
