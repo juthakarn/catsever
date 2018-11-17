d0c508256ff06573e39a28417d66a6f04d4081d6
Kittinut Pramhan <knightza123@hotmail.com>
2018-11-18 00:31:00 +0700

    server is running

---
+++ provider/service.js
@@ -0,0 +1,9 @@
+import Sequelize from 'sequelize';
+import seq from '../config/database';
+// import UserModel from '../model/user';
+
+// const User = UserModel(seq, Sequelize);
+
+// export default {
+//   User,
+// };
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 require('babel-register')({
-    presets: [ 'env' ]
-})
+  presets: ['env'],
+});
 
 // Import the rest of our application.
-module.exports = require('./app.js')
+module.exports = require('./app.js');
