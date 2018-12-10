fdbbc7d687d937799c172551da549832778f0f59
Kittinutz <knightza94@gmail.com>
2018-12-11 01:11:35 +0700

    fix authentication

--- server/controller/authenController.js
+++ server/controller/authenController.js
@@ -1,52 +1,64 @@
-import { User } from '../../provider/service';
 import jwt from 'jsonwebtoken';
+import { User } from '../../provider/service';
+
 export const signup = async (req, res) => {
-    const { name, surname, email, password } = req.body;
-    if (name && surname && email && password) {
-        const result = await User.findOne({
-            where: {
-                email: email
-            }
-        })
-        if (!result) {
-            User.create(req.body)
-            res.send('success')
-        }
-        res.send('email is use already')
+  const {
+    name, surname, email, password,
+  } = req.body;
+  if (name && surname && email && password) {
+    const result = await User.findOne({
+      where: {
+        email,
+      },
+    });
+    if (!result) {
+      User.create(req.body);
+      const response = await User.findOne({
+        where: {
+          email,
+          password,
+        },
+      });
+      const token = jwt.sign({ response }, 'cat');
+      res.send(token);
     }
-    res.send('error')
-    //console.log(name);
-    //res.send({ hello: name });
+    res.send('email is use already');
+  }
+
+  // console.log(name);
+  // res.send({ hello: name });
 };
 export const getCurUser = async (req, res) => {
-    const id = req.params.id;
-    const result = await User.findOne({
-        where: {
-            id: id
-        }
-    })
-    const token = jwt.sign({ result }, 'cat');
-    res.send(token)
+  const id = req.params.id;
+  const result = await User.findOne({
+    where: {
+      id,
+    },
+  });
+  const token = jwt.sign({ result }, 'cat');
+  res.send(token);
 };
 export const getTakeToken = (req, res) => {
-    const { token } = req.params
-    const result = jwt.verify(token, 'cat');
-    res.send(result);
+  const { token } = req.params;
+  const result = jwt.verify(token, 'cat');
+  res.send(result);
 };
 export const signin = async (req, res) => {
-    const { email, password } = req.body;
-    if (email && password) {
-        const result = await User.findOne({
-            where: {
-                email: email,
-                password: password
-            }
-        })
-        if (!result) {
-            res.send('Not regist already?Please Signup')
-        }
-        const token = jwt.sign({ result }, 'cat');
-        res.send(token)
+  const { email, password } = req.body;
+  if (email && password) {
+    const result = await User.findOne({
+      where: {
+        email,
+        password,
+      },
+    });
+    if (!result) {
+      console.log('res', result);
+      res.send('Not regist already?Please Signup');
     }
-    res.send('We cannot found your email or password')
-}
+    const token = jwt.sign({ result }, 'cat');
+    console.log('tt', token);
+    res.send(token);
+  }
+  res.send('We cannot found your email or password');
+};
